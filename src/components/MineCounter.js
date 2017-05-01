be54ddbe12937ccf38c84e29bd5f329a7d312ee7
Colin Fendrick <colin.fendrick@gmail.com>
2017-05-01 09:16:23 -0400

    removed lodash

--- src/components/MineCounter.js
+++ src/components/MineCounter.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
 import gamestate from '../stores/game'
-import _ from 'lodash'
 
 @observer
 class MineCounter extends Component {
